98696deeac9c13bbd9f3cfcc83dc6a5d33a16778
lvsheng1 <326375225@qq.com>
2022-08-12 18:02:37 +0800

    阻塞式服务端

---
+++ src/cn/nio/server/NormalServer.java
@@ -0,0 +1,34 @@
+package cn.nio.server;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @author 吕胜 lvheng1
+ * @date 2022/8/12
+ **/
+public class NormalServer {
+	private ServerSocket serverSocket;
+	
+	public NormalServer initServer(int port) {
+		try {
+			serverSocket = new ServerSocket();
+			SocketAddress socketAddress = new InetSocketAddress("127.0.0.1", port);
+			serverSocket.bind(socketAddress);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		return this;
+	}
+	
+	public void listen() throws IOException {
+		while (true) {
+			Socket socket = serverSocket.accept();
+			
+		}
+	}
+}
