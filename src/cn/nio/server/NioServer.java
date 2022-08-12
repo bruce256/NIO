e046bf3e645ad7552deb1bf4496e1eaecc2d92e4
lvsheng1 <326375225@qq.com>
2022-08-12 16:48:32 +0800

    用线程池优化

--- src/cn/nio/server/NioServer.java
+++ src/cn/nio/server/NioServer.java
@@ -8,13 +8,20 @@ import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
 import java.util.Iterator;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author LvSheng
  */
 public class NioServer {
-	private Selector	serverSelector;
-
+	
+	private Selector serverSelector;
+	
+	private int                corePoolSize = Runtime.getRuntime().availableProcessors();
+	private ThreadPoolExecutor threadPool   = new ThreadPoolExecutor(corePoolSize, corePoolSize * 2, 1, TimeUnit.MINUTES, new LinkedBlockingQueue<>(1000));
+	
 	public NioServer initServer(int port) throws IOException {
 		ServerSocketChannel serverChannel = ServerSocketChannel.open();
 		// 非阻塞模式
@@ -25,7 +32,7 @@ public class NioServer {
 		serverChannel.register(serverSelector, SelectionKey.OP_ACCEPT);
 		return this;
 	}
-
+	
 	public void startListening() throws IOException {
 		System.out.println("服务端启动成功！");
 		while (true) {
@@ -35,28 +42,32 @@ public class NioServer {
 			while (iterator.hasNext()) {
 				SelectionKey key = (SelectionKey) iterator.next();
 				// 必须删除，否则下次遍历时还会遍历旧的key
-				iterator.remove();	
+				iterator.remove();
 				if (key.isAcceptable()) {
-					accept(key);
+					threadPool.execute(() -> accept(key));
 				} else if (key.isReadable()) {
-					read(key);
+					threadPool.execute(() -> read(key));
 				}
 			}
 		}
 	}
 	
 	// 接受客户端的连接请求
-	private void accept(SelectionKey key) throws IOException {
+	private void accept(SelectionKey key) {
 		ServerSocketChannel server = (ServerSocketChannel) key.channel();
-		// 获得和客户端连接的通道
-		SocketChannel channel = server.accept();
-		// 设置成非阻塞
-		channel.configureBlocking(false);
-		channel.write(ByteBuffer.wrap(new String("客户端连接成功\n").getBytes()));
-		// 注册
-		channel.register(this.serverSelector, SelectionKey.OP_READ);
+		try {
+			// 获得和客户端连接的通道
+			SocketChannel channel = server.accept();
+			// 设置成非阻塞
+			channel.configureBlocking(false);
+			channel.write(ByteBuffer.wrap(new String("客户端连接成功\n").getBytes()));
+			// 注册
+			channel.register(this.serverSelector, SelectionKey.OP_READ);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-
+	
 	private void read(SelectionKey key) {
 		try {
 			SocketChannel channel = (SocketChannel) key.channel();
@@ -69,7 +80,7 @@ public class NioServer {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) throws IOException {
 		new NioServer().initServer(8080).startListening();
 	}
